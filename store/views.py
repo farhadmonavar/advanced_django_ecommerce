2aa503367ec11b40ed77416ab62aa139fa73c7ea
farhadmonavar <farhad.monavar@gmail.com>
2026-07-24 16:11:12 +0330

    feat(review): allow reviews only from authenticated verified buyers

--- store/views.py
+++ store/views.py
@@ -1,12 +1,15 @@
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import CartItem
+from orders.models import OrderProduct
 from .forms import ReviewForm
 from .models import Product, ReviewRating
 from category.models import Category
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.contrib import messages
+from .models import Product, ReviewRating, ProductGallery
+
 
 def store(request, category_slug=None):
     categories = None
@@ -39,13 +42,29 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
-    
     except Exception as e:
         raise e
-    
+
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
-        'in_cart': in_cart
+        'in_cart'       : in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
